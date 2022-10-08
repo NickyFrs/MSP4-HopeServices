4736bfdc4837c4e528ef6c00966c662dd8d99291
Nickyfrs <nickyfrs@gmail.com>
2022-10-08 12:09:54 +0100

    started the store models. category model created.

--- store/models.py
+++ store/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+# Store models from here.
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(active=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("store:category_list", args=[self.slug])
