d052f26e411b71855668d1336eaeb1f7ca11fbdf
Nickyfrs <nickyfrs@gmail.com>
2022-10-08 14:58:44 +0100

    views for products and categories done.

--- store/views.py
+++ store/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+# store views from here.
+
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
+
+
+def all_products(request):
+    # grab what to show
+    products = Product.objects.all()
+    return render(request, 'store/lessons/all.html', {'products': products})
+
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+
+def category_list(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    return render(request, 'store/lessons/lessons.html', {'category': category, 'products': products})
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, active=True)
+    return render(request, 'store/lessons/detail.html', {'product': product})
