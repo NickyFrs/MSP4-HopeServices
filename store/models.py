99a65cdc326ccd3f7716e4c334f79c77063700fd
Nickyfrs <nickyfrs@gmail.com>
2022-10-08 12:17:59 +0100

    product model created.

--- store/models.py
+++ store/models.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 from django.db import models
-
+from django.urls import reverse
 
 # Store models from here.
+
+
 
 class ProductManager(models.Manager):
     def get_queryset(self):
@@ -20,3 +23,29 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse("store:category_list", args=[self.slug])
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name="product", on_delete=models.CASCADE)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="product_created_by",
+                                   on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/', default='images/default.png')
+    slug = models.SlugField(max_length=255, null=False, unique=True)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+    products = ProductManager()
+
+    class Meta:
+        verbose_name_plural = 'Products'
+        ordering = ('name',)
+
+    def get_absolute_url(self):
+        return reverse("store:product_detail", args=[self.slug])
+
+    def __str__(self):
+        return self.name
