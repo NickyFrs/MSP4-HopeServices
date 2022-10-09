7a4ef13df0cdd80f98b0e6faff64acc104c5019b
Nickyfrs <nickyfrs@gmail.com>
2022-10-09 11:35:20 +0100

    register more urls for category and product views. sorted out some dependencies

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,7 @@ app_name = 'store'  # connect to the namespace in the main urls.py
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('', views.all_products, name='all_products'),
+    path('<slug:slug>', views.product_detail, name='product_detail'),
+    path('search/<slug:category_slug>/', views.category_list, name='category_list'),
 ]
