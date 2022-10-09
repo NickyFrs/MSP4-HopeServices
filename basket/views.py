66c5df7685c7f9ed07e1a149929589cd109f7ad8
Nickyfrs <nickyfrs@gmail.com>
2022-10-09 12:26:52 +0100

    started basket app

--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 
-# Create your views here.
+from store.models import Product
+from .basket import Basket
+
+
+# views from here.
+
+
+def basket_review(request):
+    basket = Basket(request) # making an instance of the class Basket in basket.py
+    return render(request, 'basket/review.html', {'basket': basket})
