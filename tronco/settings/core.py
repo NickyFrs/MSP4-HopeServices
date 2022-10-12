2869ea46622b2fd613888b3c366516b18b345ebc
Nickyfrs <nickyfrs@gmail.com>
2022-10-13 00:11:35 +0100

    email configuration test

--- tronco/settings/core.py
+++ tronco/settings/core.py
@@ -169,7 +169,7 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # EMAIL_FILE_PATH = (os.path.join())
 # ACCOUNT_ACTIVATION_DAYS = 7
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = os.environ.get('EMAIL_PORT', cast=int)
+EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
