d1479a70123518aa68a7ac23f7b63d3ecbc1c84f
Nickyfrs <nickyfrs@gmail.com>
2022-10-15 13:32:25 +0100

    testing

--- staticfiles/payment/js/main.js
+++ staticfiles/payment/js/main.js
@@ -1,28 +1,9 @@
 // stripe secret key
-let stripe = Stripe('pk_test_51Lc5tMG7qH1UlLmbPNmCk7YYGvnJyKUKGtv0SpStsTB9eENXOLluBGiSnKwVoUE3o54fkO8a1dmNB7J1eheqSodi00P7O7JWoS')
-
-
-// ajax code not working. CSRF TOKEN problems
-  // $.ajax({
-  //   type: "POST",
-  //   url: 'http://127.0.0.1:8000/orders/add/',
-  //   data: {
-  //     order_key: clientsecret,
-  //     csrfmiddlewaretoken: CSRF_TOKEN,
-  //     action: "post",
-  //   },
-  //   success: function (json) {
-  //     console.log(json.success)
-  //
-  //
-  //   },
-  //   error: function (xhr, errmsg, err) {},
-  // });
-
+let stripe = Stripe('pk_test_51Lc5tMG7qH1UlLmbPNmCk7YYGvnJyKUKGtv0SpStsTB9eENXOLluBGiSnKwVoUE3o54fkO8a1dmNB7J1eheqSodi00P7O7JWoS');
 
 
 let elem = document.getElementById('submit');
-clientsecret = elem.getAttribute('data-secret');
+let clientsecret = elem.getAttribute('data-secret');
 
 // Set up Stripe.js and Elements to use in checkout form
 let elements = stripe.elements();
@@ -76,14 +57,14 @@ form.addEventListener('submit', function(e) {
 
     $.ajax({
     type: "POST",
-    url: 'http://127.0.0.1:8000/orders/add/',
+    url: 'https://msp4-hopeservices.herokuapp.com/orders/add/',
     data: {
       order_key: clientsecret,
       csrfmiddlewaretoken: CSRF_TOKEN,
       action: "post",
     },
     success: function (json) {
-      console.log(json.success)
+      console.log(json.success);
 
     stripe.confirmCardPayment(clientsecret, {
         payment_method: {
@@ -101,16 +82,16 @@ form.addEventListener('submit', function(e) {
       ).then(
         function(result) {
         if (result.error) {
-          console.log('payment error')
+          console.log('payment error');
           console.log(result.error.message);
         } else {
           if (result.paymentIntent.status === 'succeeded') {
-            console.log('payment processed')
+            console.log('payment processed');
             // There's a risk of the customer closing the window before callback
             // execution. Set up a webhook or plugin to listen for the
             // payment_intent.succeeded event that handles any business critical
             // post-payment actions.
-            window.location.replace("http://127.0.0.1:8000/payment/orderplaced/");
+            window.location.replace("https://msp4-hopeservices.herokuapp.com/payment/orderplaced/");
           }
         }
       });
